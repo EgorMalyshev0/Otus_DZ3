26a2eb684486ca66621f9ddf1546c7c4aef0cb17
Egor Malyshev <egor.malyshev@umbrellait.com>
2021-10-03 17:53:27 +0300

    first commit

---
+++ DZ№3/DZ_3App.swift
@@ -0,0 +1,17 @@
+//
+//  DZ_3App.swift
+//  DZ№3
+//
+//  Created by Egor Malyshev on 02.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct DZ_3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
